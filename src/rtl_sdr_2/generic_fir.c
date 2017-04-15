e5d7a866a9838a768843406b9b9ebbad16d6ab7a
Adrian Chadd <adrian@freebsd.org>
2017-04-15 15:15:31 -0700

    Refactor out generic_fir routine.

---
+++ src/rtl_sdr_2/generic_fir.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#include "generic_fir.h"
+
+void generic_fir(int16_t *data, int length, int *fir, int16_t *hist)
+/* Okay, not at all generic.  Assumes length 9, fix that eventually. */
+{
+	int d, temp, sum;
+	for (d=0; d<length; d+=2) {
+		temp = data[d];
+		sum = 0;
+		sum += (hist[0] + hist[8]) * fir[1];
+		sum += (hist[1] + hist[7]) * fir[2];
+		sum += (hist[2] + hist[6]) * fir[3];
+		sum += (hist[3] + hist[5]) * fir[4];
+		sum +=	    hist[4]  * fir[5];
+		data[d] = sum >> 15 ;
+		hist[0] = hist[1];
+		hist[1] = hist[2];
+		hist[2] = hist[3];
+		hist[3] = hist[4];
+		hist[4] = hist[5];
+		hist[5] = hist[6];
+		hist[6] = hist[7];
+		hist[7] = hist[8];
+		hist[8] = temp;
+	}
+}
--- src/rtl_sdr_2/rtl_fm.c
+++ src/rtl_sdr_2/rtl_fm.c
@@ -92,6 +92,7 @@ static int atan_lut_coef = 8;
 #include "fifth_order.h"
 #include "rotate_90.h"
 #include "low_pass.h"
+#include "generic_fir.h"
 
 // multiple of these, eventually
 struct dongle_state dongle;
@@ -197,31 +198,6 @@ double log2(double n)
 	return log(n) / log(2.0);
 }
 #endif
-
-void generic_fir(int16_t *data, int length, int *fir, int16_t *hist)
-/* Okay, not at all generic.  Assumes length 9, fix that eventually. */
-{
-	int d, temp, sum;
-	for (d=0; d<length; d+=2) {
-		temp = data[d];
-		sum = 0;
-		sum += (hist[0] + hist[8]) * fir[1];
-		sum += (hist[1] + hist[7]) * fir[2];
-		sum += (hist[2] + hist[6]) * fir[3];
-		sum += (hist[3] + hist[5]) * fir[4];
-		sum +=            hist[4]  * fir[5];
-		data[d] = sum >> 15 ;
-		hist[0] = hist[1];
-		hist[1] = hist[2];
-		hist[2] = hist[3];
-		hist[3] = hist[4];
-		hist[4] = hist[5];
-		hist[5] = hist[6];
-		hist[6] = hist[7];
-		hist[7] = hist[8];
-		hist[8] = temp;
-	}
-}
 
 /* define our own complex math ops
    because ARMv5 has no hardware float */
