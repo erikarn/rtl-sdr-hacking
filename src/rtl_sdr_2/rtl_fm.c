7583f56054f7c093dbc6d618c544bf6010e0b2cf
Adrian Chadd <adrian@freebsd.org>
2017-04-14 23:35:15 -0700

    More refactoring!

--- src/rtl_sdr_2/rtl_fm.c
+++ src/rtl_sdr_2/rtl_fm.c
@@ -198,37 +198,6 @@ double log2(double n)
 }
 #endif
 
-void fifth_order(int16_t *data, int length, int16_t *hist)
-/* for half of interleaved data */
-{
-	int i;
-	int16_t a, b, c, d, e, f;
-	a = hist[1];
-	b = hist[2];
-	c = hist[3];
-	d = hist[4];
-	e = hist[5];
-	f = data[0];
-	/* a downsample should improve resolution, so don't fully shift */
-	data[0] = (a + (b+e)*5 + (c+d)*10 + f) >> 4;
-	for (i=4; i<length; i+=4) {
-		a = c;
-		b = d;
-		c = e;
-		d = f;
-		e = data[i-2];
-		f = data[i];
-		data[i/2] = (a + (b+e)*5 + (c+d)*10 + f) >> 4;
-	}
-	/* archive */
-	hist[0] = a;
-	hist[1] = b;
-	hist[2] = c;
-	hist[3] = d;
-	hist[4] = e;
-	hist[5] = f;
-}
-
 void generic_fir(int16_t *data, int length, int *fir, int16_t *hist)
 /* Okay, not at all generic.  Assumes length 9, fix that eventually. */
 {
